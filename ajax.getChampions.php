2f9b1534719680b21302b8d9611a7c7c4025b12d
jonesiken <87319607+jonesiken@users.noreply.github.com>
2021-08-13 09:49:34 +0300

    Update ajax.getChampions.php

--- ajax.getChampions.php
+++ ajax.getChampions.php
@@ -1,6 +1,6 @@
 <?php
 
-  $mysqli = new mysqli("localhost","noradrzo_827","3&Uf&R2p","noradrzo_827");
+  $mysqli = new mysqli("localhost","");
   #$query = $mysqli->query("SELECT name, lastname, score FROM `users` WHERE `score`= (SELECT MAX(`score`) FROM `users`)");
   $query = $mysqli->query("SELECT name, lastname, score FROM `users` ORDER BY `score` DESC");
   $output = '';
